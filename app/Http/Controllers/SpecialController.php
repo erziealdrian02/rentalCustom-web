b6ed4c8bbeb6e2c21e6ea5cdf3837f9ecbc08471
erziealdrian02 <erzie.aldrian02@gmail.com>
2026-03-28 19:40:19 +0700

    Add Some Search Bar and Sorting Field

--- app/Http/Controllers/SpecialController.php
+++ app/Http/Controllers/SpecialController.php
@@ -17,25 +17,24 @@ class SpecialController extends Controller
         return session('soldTools', [['id' => 1, 'toolCode' => 'TL-002', 'toolName' => 'Hammer', 'serialNumber' => 'SN-HM-002', 'replacementValue' => 30.0, 'soldPrice' => 15.0, 'soldDate' => '2025-01-12'], ['id' => 2, 'toolCode' => 'TL-005', 'toolName' => 'Screwdriver', 'serialNumber' => 'SN-SD-005', 'replacementValue' => 20.0, 'soldPrice' => 10.0, 'soldDate' => '2025-01-14'], ['id' => 3, 'toolCode' => 'TL-008', 'toolName' => 'Measuring Tape', 'serialNumber' => 'SN-MT-008', 'replacementValue' => 15.0, 'soldPrice' => 8.0, 'soldDate' => '2025-01-19'], ['id' => 4, 'toolCode' => 'TL-010', 'toolName' => 'Drill Bit Set', 'serialNumber' => 'SN-DB-010', 'replacementValue' => 45.0, 'soldPrice' => 30.0, 'soldDate' => '2025-01-21']]);
     }
 
-    public function lostTools(Request $request)
-    {
-        $perPage = in_array($request->per_page, [10, 50, 100]) ? $request->per_page : 10;
-        $lostTools = StockMovement::where('stock_type', 'LOST')->paginate($perPage);
-
-        return view('special.lostTools', compact('lostTools'));
-    }
-
     public function soldTools(Request $request)
     {
-        $perPage = in_array($request->per_page, [10, 50, 100]) ? $request->per_page : 10;
-        $soldTools = StockMovement::where('stock_type', 'SOLD')->paginate($perPage);
-        $totalSold = count($soldTools);
-        // dd($soldTools);
+        $soldTools = StockMovement::where('stock_type', 'SOLD')
+            ->with(['tool'])
+            ->get();
 
-        $totalRevenue = $soldTools->sum(function ($item) {
-            return ($item->tool->replacement_value ?? 0) * $item->quantity;
-        });
+        $totalSold = $soldTools->sum('quantity');
+        $totalRevenue = $soldTools->sum(fn($item) => ($item->selling_price ?? 0) * $item->quantity);
 
         return view('special.soldTools', compact('soldTools', 'totalSold', 'totalRevenue'));
+    }
+
+    public function lostTools(Request $request)
+    {
+        $lostTools = StockMovement::where('stock_type', 'LOST')
+            ->with(['tool', 'rent.customer'])
+            ->get();
+
+        return view('special.lostTools', compact('lostTools'));
     }
 }
